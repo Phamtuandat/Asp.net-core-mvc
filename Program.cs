d45f72e1377c9350260d83d11cfd3a41658894ed
Phamdat\Pham Tuan Dat <phamtuandat1a0@gmail.com>
2023-05-15 08:27:39 +0700

    intit

---
+++ Program.cs
@@ -0,0 +1,95 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("MVCIdentityDbContextConnection") ?? throw new InvalidOperationException("Connection string 'MVCIdentityDbContextConnection' not found.");
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AppDbContext>();
+builder.Services.AddDefaultIdentity<AppUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<AppDbContext>().AddDefaultTokenProviders();
+builder.Services.AddRazorPages();
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    // Password settings.
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireNonAlphanumeric = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequiredLength = 6;
+    options.Password.RequiredUniqueChars = 1;
+
+    // Lockout settings.
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+    options.Lockout.MaxFailedAccessAttempts = 5;
+    options.Lockout.AllowedForNewUsers = true;
+
+    // User settings.
+    options.User.AllowedUserNameCharacters =
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+    options.User.RequireUniqueEmail = false;
+});
+
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    // Cookie settings
+    options.Cookie.HttpOnly = true;
+    options.ExpireTimeSpan = TimeSpan.FromMinutes(5);
+
+    options.LoginPath = "/Identity/Account/Login";
+    options.AccessDeniedPath = "/Identity/Account/AccessDenied";
+    options.SlidingExpiration = true;
+});
+builder.Services
+        .AddAuthentication()
+        .AddGoogle(option =>
+        {
+            var gconfig = builder.Configuration.GetSection("Authentication:Google");
+            option.ClientId = gconfig["ClientId"];
+            option.ClientSecret = gconfig["ClientSecrect"];
+            option.CallbackPath = "/login-with-google";
+
+        });
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.AddStatusCodePage();
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();   // Phục hồi thông tin đăng nhập (xác thực)
+app.UseAuthorization();
+app.MapRazorPages();
+app.MapAreaControllerRoute(
+    name: "database",
+    pattern: "/{controller}/{action=Index}/{id?}",
+    areaName: "Database"
+);
+app.MapAreaControllerRoute(
+    name: "contact",
+    pattern: "/{controller}/{action=Index}/{id?}",
+    areaName: "Contact"
+);
+app.MapAreaControllerRoute(
+    name: "identity",
+    pattern: "/{controller}/{action=Index}/{id?}",
+    areaName: "Identity"
+);
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "/{controller=Home}/{action=Index}/{id?}");
+app.Run();
