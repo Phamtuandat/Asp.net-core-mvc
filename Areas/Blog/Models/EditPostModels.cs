a15302ea1edb85bcc8e0a42c7212951dcee34acd
Phamdat\Pham Tuan Dat <phamtuandat1a0@gmail.com>
2023-05-25 21:26:57 +0700

    remove cache

--- Areas/Blog/Models/EditPostModels.cs
+++
@@ -1,9 +0,0 @@
-using App.Models;
-
-namespace App.Areas.Blog.Models
-{
-    public class EditPostModels : CreatePostModels
-    {
-        public int PostId { get; set; }
-    }
-}
--- Data/RoleNames.cs
+++
@@ -1,9 +0,0 @@
-namespace App.Data
-{
-    public class RoleNames
-    {
-        public const string Administrator = "Administrator";
-        public const string Editor = "Editor";
-        public const string Member = "Member";
-    }
-}
